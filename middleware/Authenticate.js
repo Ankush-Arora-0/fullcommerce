00f0c895bee7bdec53e8906658af6f5b9d112d80
Ankush-Arora-0 <ankushsatnam@gmail.com>
2023-07-14 15:06:24 +0530

    first commit

--- middleware/Authenticate.js
+++ middleware/Authenticate.js
@@ -4,7 +4,9 @@ import User from '../model/userSchema.js';
 const Authenticate = async(req,res,next)=>{
     try{
         const token = req.cookies.jwtoken;
+        console.log(token);
         const verifyToken= jwt.verify(token,process.env.SECRET_KEY);
+        console.log(process.env.SECRET_KEY);
         const rootUser= await User.findOne({_id:verifyToken._id,"tokens.token":token});
         if(!rootUser){
             throw new Error('user not found')
